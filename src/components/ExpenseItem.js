7cb9465201e629a46533e466737c84b4cd1ecdf6
Alberto Rojas <shaka0241@gmail.com>
2022-10-31 23:37:15 -0300

    init expense item

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,16 @@
+
+function ExpenseItem () {
+    return (
+        <div>
+            <div>
+                Octubre 31 de 2022 
+            </div>
+            <div>
+                <h2>Carro Deportivo</h2>
+                <div>$300</div>
+            </div>
+        </div>
+    );
+}
+
+export default ExpenseItem;
